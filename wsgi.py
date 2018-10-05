16220f1d92dcf53d8da68911ffb9a3ed6eae256a
sapkotaruz11 <38007470+sapkotaruz11@users.noreply.github.com>
2018-10-05 20:40:38 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os, sys
-sys.path.append('/home/rupezzz111/Mypostapp/')
+import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from django.core.wsgi import get_wsgi_application
